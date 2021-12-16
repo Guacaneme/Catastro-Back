01eb0dd6fdbbf42f55b725b31386e84431e8dde7
lguacaneme <lguacaneme@unal.edu.co>
2021-12-16 04:34:20 -0500

    Modelo usuarios

---
+++ modelos/user_model.js
@@ -0,0 +1,29 @@
+const { genSalt, hash } = require('bcrypt');
+const {Schema,model} = require('mongoose');
+
+const user_esquema = new Schema({
+    correo:{
+        type: "string",
+        unique:true,
+        required:true,
+        max:100
+    },
+    contrasena:{
+        type: "string",
+        required:true
+    },
+    rol:{
+        type: "string",
+        required:true
+    }
+});
+
+user_esquema.pre("/guardar_use", function(next) {
+    const salt = genSalt(10);
+    console.log(salt);
+    this.contrasena= hash(this.contrasena, salt);
+    next();
+});
+
+const user_model = model("usuarios",user_esquema);
+exports.user_model=user_model;
